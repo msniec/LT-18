1de83c50ed92e70a1fa0e2082cd662b95318c8dc
msniec <michal.sniec@gmail.com>
2021-07-22 11:31:46 +0200

    Better questions

--- src/questions.js
+++ src/questions.js
@@ -1,12 +1,10 @@
-import React, { useState } from 'react'
-import {
-  withRouter
-} from 'react-router-dom'
+import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
 
 const styles = {
   outer: {
     width: "100vw",
-    height: "100vh"
+    height: "100vh",
   },
   question: {
     height: "50%",
@@ -14,7 +12,7 @@ const styles = {
     alignItems: "center",
     justifyContent: "center",
     fontSize: "2rem",
-    textAlign: "center"
+    textAlign: "center",
   },
   actionContainer: {
     height: "50%",
@@ -37,140 +35,153 @@ const styles = {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    color: "white"
-  }
-}
+    color: "white",
+  },
+};
 
 function Question(props) {
   const goNext = (column, row) => () => {
-    setQuestionColumn(column)
-    setQuestionRow(row)
-  }
+    setQuestionColumn(column);
+    setQuestionRow(row);
+  };
   const showAnswer = (answer) => () => {
-    props.history.push(`/result/${answer}`)
-  }
+    props.history.push(`/result/${answer}`);
+  };
 
-  const [questionRow, setQuestionRow] = useState(0)
-  const [questionColumn, setQuestionColumn] = useState(0)
+  const [questionRow, setQuestionRow] = useState(0);
+  const [questionColumn, setQuestionColumn] = useState(0);
   const questions = [
     [
       {
-        question: "Czasem przejmujesz się że nie masz wystarcząjących kompetencji by być tu gdzie jesteś?",
+        question: "Czy kiedykolwiek obawiasz się, że zostaniesz odkryty??",
         yesAction: goNext(0, 1),
         noAction: goNext(1, 1),
-      }
+      },
     ],
     [
       {
-        question: "Uważasz innych za bardziej dojrzałych do zadania od siebie",
+        question:
+          "Czy masz wrażenie, że Twoi koledzy są bardziej kompetentni od Ciebie?",
         yesAction: goNext(0, 2),
-        noAction: goNext(1, 2)
+        noAction: goNext(1, 2),
       },
       {
-        question: "Czujesz się niekomfortowo gdy ktoś chwali twoją pracę",
+        question: "Czy czujesz się niekomfortowo, gdy ktoś chwali Twoją pracę?",
         yesAction: goNext(2, 2),
-        noAction: goNext(3, 2)
-      }
+        noAction: goNext(3, 2),
+      },
     ],
     [
       {
-        question: "Używasz języka 'minimalizującego' jak np. 'wydaje mi sie', 'chyba'",
+        question:
+          'Czy używasz minimalizującego języka? (np. "Chyba", "Wydaje mi się")',
         yesAction: goNext(0, 3),
-        noAction: goNext(1, 3)
+        noAction: goNext(1, 3),
       },
       {
-        question: "Unikasz proszenia o podwyżkę",
+        question: "Czy unikasz proszenia o podwyżkę",
         yesAction: goNext(2, 3),
         noAction: goNext(3, 3),
       },
       {
-        question: "Stawiasz sobie nieosiągalne cele",
+        question: "Czy stawiasz sobie nieosiągalne cele?",
         yesAction: goNext(4, 3),
-        noAction: goNext(3, 3)
+        noAction: goNext(3, 3),
       },
       {
-        question: "Czy uważany jesteś za perfekcjoniste",
+        question: "Czy oczekujesz, że Twoja że twoja praca będzie bezbłędna?",
         yesAction: goNext(4, 3),
-        noAction: goNext(5, 3)
+        noAction: goNext(5, 3),
       },
     ],
     [
       {
-        question: "Uważasz że ludzie prawią tobie pochwały tylko bo są mili",
+        question:
+          "Czy czujesz, że mówiąc pochwały ludzie starają się być mili?",
         yesAction: goNext(1, 4),
-        noAction: goNext(0, 4)
+        noAction: goNext(0, 4),
       },
       {
-        question: "Uważasz że twój sukces jest głownie dziełem zewnętrznych czynników (dobre kontakty, zbieg okoliczności)",
+        question:
+          "Czy uważasz, że twoje sukcesy zawdzięczasz tylko czynnikom zewnętrznym (np. dobre kontakty, szczęście)?",
         yesAction: goNext(1, 4),
-        noAction: goNext(2, 4)
+        noAction: goNext(2, 4),
       },
       {
-        question: "Uważasz że twoja praca musi być bezbłedna cały czas",
+        question:
+          "Czy czujesz, że że twoja praca musi być bezbłędna przez cały przez cały czas?",
         yesAction: goNext(3, 4),
-        noAction: goNext(2, 4)
+        noAction: goNext(2, 4),
       },
       {
-        question: "Uważasz że twoje miejsce w życiu zawdzięczasz głównie szczęściu?",
+        question:
+          "Czy uważasz, że twoja pozycja w życiu jest głównie dzięki szczęściu?",
         yesAction: goNext(3, 4),
-        noAction: goNext(4, 4)
-      },
-      {
-        question: "Martwisz się że nie spełniasz postawionych wymagań",
-        yesAction: goNext(3, 4),
-        noAction: goNext(4, 4)
-      },
-      {
-        question: "Czy czętso się przepracowujesz?",
         noAction: goNext(4, 4),
-        yesAction: goNext(5, 4)
-      }
+      },
+      {
+        question: "Czy ciągle martwisz się, że nie spełniasz oczekiwań?",
+        yesAction: goNext(3, 4),
+        noAction: goNext(4, 4),
+      },
+      {
+        question:
+          "Czy często przepracowywać się lub nadmiernie się przygotowujesz?",
+        noAction: goNext(4, 4),
+        yesAction: goNext(5, 4),
+      },
     ],
     [
       {
-        question: "Czy unikasz zkładania CV do firm chyba że spełniasz wszystkie wymagania?",
+        question:
+          "Czy unikasz ubiegania się o pracę, chyba że spełniasz wszystkie wymagania??",
         yesAction: showAnswer(0),
-        noAction: showAnswer(1)
+        noAction: showAnswer(1),
       },
       {
-        question: "Uważasz że poniosłeś porażkę jeśli nauka czegoś nie idzie ci jak z płatka?",
+        question:
+          "Czy czujesz, że ponisłeś porażkę, jeśli z trudem coś opanowywujesz?",
         yesAction: showAnswer(0),
-        noAction: showAnswer(2)
+        noAction: showAnswer(2),
       },
       {
-        question: "Masz wrażenie że do wszystkiego musisz dojść sam",
+        question: "Czy czujesz się zakłopotany, kiedy nie znasz odpowiedzi?",
         yesAction: showAnswer(2),
-        noAction: showAnswer(1)
-
+        noAction: showAnswer(1),
       },
       {
-        question: "Często zostajesz w pracy do późna?",
+        question: "Czy często zostajesz do późna w pracy?",
         yesAction: showAnswer(3),
-        noAction: showAnswer(4)
+        noAction: showAnswer(4),
       },
       {
-        question: "Masz problem z delegowaniem zadań?",
+        question: "Czy masz problemy z delegowaniem zadań?",
         yesAction: showAnswer(3),
-        noAction: showAnswer(5)
+        noAction: showAnswer(5),
       },
       {
         question: "Czy jesteś mikromenadżerem?",
         yesAction: showAnswer(4),
-        noAction: showAnswer(5)
-      }
-    ]
-  ]
+        noAction: showAnswer(5),
+      },
+    ],
+  ];
 
-  const { question, yesAction, noAction } = questions[questionRow][questionColumn]
-  return <div style={styles.outer}>
-    <div style={styles.question}>
-      {question}
+  const { question, yesAction, noAction } =
+    questions[questionRow][questionColumn];
+  return (
+    <div style={styles.outer}>
+      <div style={styles.question}>{question}</div>
+      <div style={styles.actionContainer}>
+        <div style={styles.yesContainer} onClick={yesAction}>
+          Tak
+        </div>
+        <div style={styles.noContainer} onClick={noAction}>
+          Nie
+        </div>
+      </div>
     </div>
-    <div style={styles.actionContainer}>
-      <div style={styles.yesContainer} onClick={yesAction}>Tak</div>
-      <div style={styles.noContainer} onClick={noAction}>Nie</div>
-    </div>
-  </div>
+  );
 }
 
-export default withRouter(Question)
+export default withRouter(Question);
